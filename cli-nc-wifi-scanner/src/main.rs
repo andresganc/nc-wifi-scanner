166d471c11232fd5a598c5a87d841e2a0c6f3ffd
Andres Giraldo <andresganc@gmail.com>
2026-02-27 12:38:00 -0500

    MOD: CLI NC Wifi Scanner: Test example

--- cli-nc-wifi-scanner/src/main.rs
+++ cli-nc-wifi-scanner/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+
+use rswifi::{AkmType, AuthAlg, CipherType, Error, IFaceStatus, Profile, Result, WiFiInterface as _, WifiUtil};
+
+fn main() -> Result<()> {
+    let util = WifiUtil::new()?;
+    let ifaces = util.interfaces()?;
+    let iface = ifaces.first()
+        .ok_or(Error::Other("No iface found".into()))?;
+
+    // Disconnect
+    iface.disconnect()?;
+    std::thread::sleep(std::time::Duration::from_millis(100));
+    let status = iface.status()?;
+    assert_eq!(status, IFaceStatus::Disconnected);
+
+    // Add Profle
+    let ssid = "TestSSID";
+    let key = "12345678";
+    let mut profile = Profile::new(ssid)
+        .with_key(Some(key.into()))
+        .with_auth(AuthAlg::Open)
+        .with_cipher(CipherType::Ccmp);
+    profile.add_akm(AkmType::Wpa2Psk);
+    iface.add_network_profile(&profile)?;
+
+    // Scan
+    iface.scan()?;
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    let results = iface.scan_results()?;
+    for result in results {
+        if result.ssid() == ssid {
+            println!("Scanned ssid: {}", ssid);
+        }
+    }
+
+    // Connect
+    let ret = iface.connect(ssid)?;
+    assert!(ret);
+    std::thread::sleep(std::time::Duration::from_millis(500));
+    let status = iface.status()?;
+    assert_eq!(status, IFaceStatus::Connected);
+
+    // Remove Profile
+    iface.remove_network_profile(&ssid)?;
+
+    // Get Profile names
+    let results = iface.network_profile_name_list()?;
+    println!("{:?}", results);
+
+    Ok(())
 }
